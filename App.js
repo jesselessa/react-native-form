6af8e51864c90d243524724aa2e559508c64bf6f
jess <jessica.elessa@gmail.com>
2022-03-02 14:38:38 +0100

    created form with login requirements

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { TouchableOpacity, TextInput } from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [auth, setAuth] = useState(false);
+
+  const handlePress = () => {
+    if (email.length > 5 && password.length > 5) {
+      setAuth(true);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your email"
+        onChangeText={setEmail}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your password"
+        secureTextEntry
+        onChangeText={setPassword}
+      />
+      <TouchableOpacity onPress={handlePress}>
+        <Text>Submit</Text>
+      </TouchableOpacity>
+      {auth ? (
+        <View>
+          <Text>Form submitted</Text>
+        </View>
+      ) : (
+        <View>
+          <Text>You must enter a valid email and a valid password</Text>
+        </View>
+      )}
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +47,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  input: {
+    borderWidth: 1,
+    borderRadius: 10,
+    marginBottom: 5,
+    padding: 10,
   },
 });
